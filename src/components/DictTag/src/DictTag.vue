f2f4d0292ccb8135736f1d354645149529366780
黄兆琦 <1361001127@qq.com>
2024-07-26 10:32:13 +0800

    feat: DictTag组件兼容传入多个字典值展示多个标签的情况

--- src/components/DictTag/src/DictTag.vue
+++ src/components/DictTag/src/DictTag.vue
@@ -3,7 +3,7 @@ import { defineComponent, PropType, computed } from 'vue'
 import { isHexColor } from '@/utils/color'
 import { ElTag } from 'element-plus'
 import { DictDataType, getDictOptions } from '@/utils/dict'
-import { isArray, isString, isNumber, isBoolean } from '@/utils/is'
+import { isArray, isString, isNumber } from '@/utils/is'
 
 export default defineComponent({
   name: 'DictTag',
@@ -29,21 +29,20 @@ export default defineComponent({
   },
   setup(props) {
     const valueArr: any = computed(() => {
-      // 1. 是 Number 类型和 Boolean 类型的情况
-      if (isNumber(props.value) || isBoolean(props.value)) {
+      // 1.是Number类型的情况
+      if (isNumber(props.value)) {
         return [String(props.value)]
       }
-      // 2. 是字符串（进一步判断是否有包含分隔符号 -> props.sepSymbol ）
+      // 2.是字符串（进一步判断是否有','）
       else if (isString(props.value)) {
-        return props.value.split(props.separator)
+        return props.value.includes(',') ? props.value.split(',') : [String(props.value)]
       }
-      // 3. 数组
+      // 3.数组
       else if (isArray(props.value)) {
         return props.value.map(String)
       }
-      return []
     })
-    const renderDictTag = () => {
+    const rederDictTag = () => {
       if (!props.type) {
         return null
       }
@@ -54,15 +53,7 @@ export default defineComponent({
       const dictOptions = getDictOptions(props.type)
 
       return (
-        <div
-          class="dict-tag"
-          style={{
-            display: 'inline-flex',
-            gap: props.gutter,
-            justifyContent: 'center',
-            alignItems: 'center'
-          }}
-        >
+        <div class="dict-tag">
           {dictOptions.map((dict: DictDataType) => {
             if (valueArr.value.includes(dict.value)) {
               if (dict.colorType + '' === 'primary' || dict.colorType + '' === 'default') {
@@ -72,7 +63,7 @@ export default defineComponent({
                 // 添加标签的文字颜色为白色，解决自定义背景颜色时标签文字看不清的问题
                 <ElTag
                   style={dict?.cssClass ? 'color: #fff' : ''}
-                  type={dict?.colorType || null}
+                  type={dict?.colorType}
                   color={dict?.cssClass && isHexColor(dict?.cssClass) ? dict?.cssClass : ''}
                   disableTransitions={true}
                 >
